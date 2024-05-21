1b01299f00bc9ec6d9e998ec9025c3434ebd2ef0
Gyeoulie <light8013@gmail.com>
2024-05-22 00:38:15 +0800

    edit modal certificate

--- app/Livewire/Admin/Certificates/CertificatesEditModal.php
+++ app/Livewire/Admin/Certificates/CertificatesEditModal.php
@@ -2,10 +2,71 @@
 
 namespace App\Livewire\Admin\Certificates;
 
+use App\Models\Certificate_Type;
+use Illuminate\Validation\Rule;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class CertificatesEditModal extends Component
 {
+    public $certId;
+    public $certCodeInput;
+    public $certNameInput;
+
+    public function rules()
+    {
+        return [
+            'certId' => ['required'],
+            'certCodeInput' => ['required', 'string', Rule::unique('certificate_type', 'cert_Code')->ignore($this->certId, 'cert_type_id')],
+            'certNameInput' => ['required'],
+        ];
+    }
+
+    public function updateCert()
+    {
+        $this->validate();
+
+        try {
+            // Create the user record
+            Certificate_Type::where('cert_type_id', $this->certId)->update([
+                'cert_Code' => strtoupper($this->certCodeInput),
+                'cert_Name' => $this->certNameInput,
+            ]);
+
+            toastr()->success('Certificate Updated!');
+        } catch (\Exception $e) {
+
+            // Show error toastr notification
+            toastr()->error('There was an Error');
+        }
+        $this->close();
+
+        $this->dispatch('reload-table');
+    }
+
+    public function close()
+    {
+        $this->reset();
+        $this->dispatch('close');
+    }
+
+    #[On('edit-cert')]
+    public function editCert($id)
+    {
+
+        $certType = Certificate_Type::find($id);
+
+        if ($certType) {
+            $this->certId = $certType->cert_type_id;
+            $this->certCodeInput = $certType->cert_Code;
+            $this->certNameInput = $certType->cert_Name;
+            $this->dispatch('open-modal', 'edit-certificates-admin-modal');
+        } else {
+            toastr()->error('Could not fetch data');
+        }
+
+    }
+
     public function render()
     {
         return view('livewire.admin.certificates.certificates-edit-modal');
