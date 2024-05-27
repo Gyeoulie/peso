f1f64a4cc4be1b28c01c9c2f14228c1ef2a072ce
Gyeoulie <light8013@gmail.com>
2024-05-27 17:40:30 +0800

    interview modal

--- resources/views/livewire/employer/jobpost/job-applicants.blade.php
+++ resources/views/livewire/employer/jobpost/job-applicants.blade.php
@@ -432,7 +432,9 @@
                                                     </div>
 
                                                     <div x-data="{ tooltip: 'For Interview' }">
-                                                        <button x-tooltip="tooltip" type="button"
+                                                        <button x-data=""
+                                                        x-on:click.prevent="$dispatch('open-modal', 'interview-modal')"
+                                                         x-tooltip="tooltip" type="button"
                                                             class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center">
                                                             <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg"
                                                                 fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -482,7 +484,7 @@
 
                     {{-- pagination --}}
                     <div>
-
+                        {{ $applicants['list']->links() }}
                     </div>
 
 
@@ -490,3 +492,63 @@
             @endif
         </div>
     </div>
+
+
+
+
+
+
+
+    <x-modal name="interview-modal" focusable>
+        <div class="w-full max-w-4xl px-6 py-6 items-center border-b">
+            <h2 class="text-lg font-medium text-gray-900">
+                {{ __('Are you sure you want to interivew?') }}
+            </h2>
+            <hr>
+            <div class="flex flex-col mt-2">
+                <div class="flex flex-col mt-2 w-full">
+                    <x-input-label :value="__('Remarks')" />
+                    <textarea wire:model='remarks' id="message" rows="4"
+                        class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+                        placeholder="Write your remarks here..."></textarea>
+                    <x-input-error :messages="$errors->get('remarks')" class="mt-2" />
+                </div>
+
+            </div>
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button  type="button">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-primary-button  class="ms-3" type="button">
+
+
+                    {{ __('Approve Interview') }}
+
+                    <div wire:loading.delay.long role="status">
+                        <svg aria-hidden="true" class="w-4 h-4 text-gray-200 animate-spin fill-blue-600 ml-4"
+                            viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                fill="currentColor" />
+                            <path
+                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                fill="currentFill" />
+                        </svg>
+                        <span class="sr-only">Loading...</span>
+                    </div>
+                </x-primary-button>
+
+            </div>
+        </div>
+    </x-modal>
+
+
+
+
+
+
+
+
+
+</div>
