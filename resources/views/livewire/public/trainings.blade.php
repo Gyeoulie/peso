edd84ecc0150f94705ed47f3badb67b933a58d7c
Gyeoulie <light8013@gmail.com>
2024-09-12 11:48:47 +0800

    session and trainings

--- resources/views/livewire/public/trainings.blade.php
+++ resources/views/livewire/public/trainings.blade.php
@@ -413,7 +413,10 @@
                                                         Registered Date
                                                     </th>
                                                     <th scope="col" class="px-6 py-3 w-md">
-                                                        Status
+                                                        Registration Status
+                                                    </th>
+                                                    <th scope="col" class="px-6 py-3 w-md">
+                                                        Program Status
                                                     </th>
                                                     <th scope="col" class="px-6 py-3 w-md">
                                                         Action
@@ -486,27 +489,55 @@
                                                                 </div>
                                                             </td>
 
+                                                            <td class="px-6 py-4">
+                                                                <div class="flex items-center">
+                                                                    @if ($data->programs->program_Status === 'ACTIVE')
+                                                                        <div
+                                                                            class="h-2.5 w-2.5 rounded-full bg-green-500 me-2">
+                                                                        </div>
+                                                                        ACTIVE
+                                                                    @elseif ($data->programs->program_Status === 'CLOSED')
+                                                                        <div
+                                                                            class="h-2.5 w-2.5 rounded-full bg-cyan-500 me-2">
+                                                                        </div>
+                                                                        CLOSED
+                                                                    @elseif ($data->programs->program_Status === 'COMPLETED')
+                                                                        <div
+                                                                            class="h-2.5 w-2.5 rounded-full bg-blue-500 me-2">
+                                                                        </div>
+                                                                        COMPLETED
+                                                                    @elseif($data->programs->program_Status === 'CANCELLED')
+                                                                        <div
+                                                                            class="h-2.5 w-2.5 rounded-full bg-red-500 me-2">
+                                                                        </div>
+                                                                        CANCELLED
+                                                                    @endif
+                                                                </div>
+                                                            </td>
+
 
 
                                                             <td class="px-6 py-4">
                                                                 <div class="flex flex-row  gap-5">
-                                                                    <div x-data="{ tooltip: 'View Training' }">
-                                                                        <a wire:navigate
-                                                                            href="{{ route('training.show', ['id' => $data->programs->program_id]) }}"
-                                                                            x-tooltip="tooltip" type="button"
-                                                                            class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center">
-                                                                            <svg class="h-5 w-5"
-                                                                                xmlns="http://www.w3.org/2000/svg"
-                                                                                viewBox="0 0 24 24"
-                                                                                fill="currentColor">
-                                                                                <path
-                                                                                    d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                                                                                <path fill-rule="evenodd"
-                                                                                    d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
-                                                                                    clip-rule="evenodd" />
-                                                                            </svg>
-                                                                        </a>
-                                                                    </div>
+                                                                    @if ($data->programs->program_Status === 'ACTIVE')
+                                                                        <div x-data="{ tooltip: 'View Training' }">
+                                                                            <a wire:navigate
+                                                                                href="{{ route('training.show', ['id' => $data->programs->program_id]) }}"
+                                                                                x-tooltip="tooltip" type="button"
+                                                                                class="text-blue-700 border border-blue-700 hover:bg-blue-700 hover:text-white focus:ring-2 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center">
+                                                                                <svg class="h-5 w-5"
+                                                                                    xmlns="http://www.w3.org/2000/svg"
+                                                                                    viewBox="0 0 24 24"
+                                                                                    fill="currentColor">
+                                                                                    <path
+                                                                                        d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
+                                                                                    <path fill-rule="evenodd"
+                                                                                        d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z"
+                                                                                        clip-rule="evenodd" />
+                                                                                </svg>
+                                                                            </a>
+                                                                        </div>
+                                                                    @endif
 
 
                                                                     <div x-data="{ tooltip: 'View Ticket' }">
