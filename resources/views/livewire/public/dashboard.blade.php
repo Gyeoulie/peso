6e5e1e92f300b5a43c8c3e4f88ece41ac15662e8
Gyeoulie <light8013@gmail.com>
2024-09-26 03:29:13 +0800

    dashboard fix

--- resources/views/livewire/public/dashboard.blade.php
+++ resources/views/livewire/public/dashboard.blade.php
@@ -566,7 +566,7 @@
                         </div>
 
                         {{-- LICENSE SEARCH --}}
-                        <input wire:model.live='search' type="search"
+                        <input wire:model.live='searchIndustry' type="search"
                             class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full sm:w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                             placeholder="Search industry">
                     </div>
@@ -714,7 +714,7 @@
                             </svg>
                         </div>
 
-                        <input type="search" wire:model.live='search'
+                        <input type="search" wire:model.live='searchTags'
                             class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-full sm:w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
                             placeholder="Search Job Position">
                     </div>
