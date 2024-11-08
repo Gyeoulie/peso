42727f72f4cb3af882a77d9c8a2fedfdb461a9ce
Gyeoulie <light8013@gmail.com>
2024-11-09 03:29:25 +0800

    revisions

--- app/Mail/ConfirmTraining.php
+++ app/Mail/ConfirmTraining.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class ConfirmTraining extends Mailable
+class ConfirmTraining extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
